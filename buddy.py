c4939ee022ce3934fdc2fcb9db6709977366da12
manika tyagi <94793244+manikatyagi@users.noreply.github.com>
2022-04-28 12:35:03 +0530

    Update buddy.py

--- buddy.py
+++ buddy.py
@@ -1,5 +1,7 @@
 import ctypes
+from queue import PriorityQueue
 import shutil
+from numpy import take
 import pyautogui
 import pyttsx3
 import speech_recognition as sr
@@ -9,11 +11,13 @@ import webbrowser
 import pywhatkit
 import os
 import smtplib
+import winshell
 import pyjokes
 import wolframalpha
 import json
 from GoogleNews import GoogleNews
-from urllib.request import urlopen
+from urllib.request import URLopener
+from pywinauto import application
 
 #Initializing voices and googlenews and engine
 googlenews = GoogleNews()
@@ -55,6 +59,16 @@ def username(): #Interact with user to know their Name
     print(("Welcome Mr. " + uname).center(columns))
     print("#####################".center(columns))
     speak("Please tell me How can i Help you" + uname)
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+     
+    # Enable low security in gmail
+    server.login('jainakshat504@gmail.com', 'lctz pxel erpa ekry')      
+    server.sendmail('jainakshat504@gmail.com', to, content)
+    server.close()
 
 
 def takeCommand(): 
@@ -108,9 +122,15 @@ if _name_ == "_main_":
             speak(day)
             speak(month)
             speak(year)
+            a=str(day)
+            b=str(month)
+            c=str(year)
+            print("Today's date is: "+a+"/"+b+"/"+c)
+    
         
         elif 'thanks' in query:
             speak("Thanks for giving me your time, Will See you again")
+            print("Thanks for giving me your time, Will See you again")
             break
 
         elif 'open youtube' in query:
@@ -137,16 +157,40 @@ if _name_ == "_main_":
             query = query.replace("search", "")
             query = query.replace("play", "")
             speak("This is what I found, for u Sir")
+            print("This is what I found, for u Sir")
             webbrowser.open(query)
 
         elif "screenshot" in query:
             speak("Taking Screenshot")
+            print("Taking Screenshot")
             ss = pyautogui.screenshot()
-            ss.save(r'E:\Screenshots\image_1.png')
+            ss.save(r'E:\\Screenshots\\Image1.png')
             speak("Boss,Your Screenshot has been Successfully, saved")
+            print("Boss,Your Screenshot has been Successfully, saved")
+        
+        elif 'send a mail' in query:
+            try:
+                speak("What should I say?")
+                print("What should I say?")
+                content = takeCommand()
+                print(content)
+                #speak("whome should i send")
+                to = 'mogalapujayasrikar53@gmail.com'
+                sendEmail(to, content)
+                speak("Email has been sent !")
+                print("Email has been sent !")
+            except Exception as e:                       
+                print(e)
+                speak("I am not able to send this email")
+
+        elif "open bin" in query:
+            os.startfile(r"C:\$RECYCLE.BIN ")
+
+        elif "open drive" in query:
+            os.startfile(r"C:")
 
         elif 'open google' in query:
-            webbrowser.open("google.com")
+            webbrowser.open("google.com")     
 
         elif 'open vtop' in query:
             webbrowser.open("vtop.vit.ac.in")
@@ -155,21 +199,24 @@ if _name_ == "_main_":
             codepPath = "C:\\Users\\91882\\AppData\\Local\\Microsoft\\Teams\\current\\Teams.exe"
             os.startfile(codepPath)
 
-        elif 'open teams' in query:
-            codepPath = "C:\\Users\\91882\\AppData\\Local\\Microsoft\\Teams\\current\\Teams.exe"
-            os.startfile(codepPath)
+        elif 'open word' in query:
+            app = application.Application()
+            app.start("write.exe")
+            print("Opening Word for you!!")
+
+        elif 'open notepad' in query:
+            app = application.Application()
+            app.start("notepad.exe")
+            print("Opening Notepad ")
+
+        elif 'open calculator' in query:
+            app = application.Application()
+            app.start("calc.exe")
+            print("Opening Calculator ")
         
-        elif 'D drive' in query:
-            codepPath = "D:\\AI Class Materials"
-            os.startfile(codepPath)
-
-        elif 'E drive' in query:
-            codepPath = "E:\\"
-            os.startfile(codepPath)
-
-
         elif 'joke' in query:
             speak(pyjokes.get_joke())
+            print(pyjokes.get_joke())
         
         elif "how are you" in query or "What's up" in query:
             speak("I'm very well dude, Thanks for Asking")
@@ -225,7 +272,7 @@ if _name_ == "_main_":
         
 #Checks for what, who and Calculate to filter questions 
 
-        elif "what is" in query or "who is" in query or "Calculate" in query:
+        elif "what is" in query or "who is" in query or "calculate" in query:
             client = wolframalpha.Client("YXYH4V-2YJ7RJKEV9")
             res = client.query(query)
         #Exception - If answer not found or Network error or Voice Unrecognized
